797ca6e1487a0a0b8a6cf1fd93350d6d584f61f8
ph4n70m-nuk3r <ph4n70m.nuk3r@gmail.com>
2025-07-19 10:45:25 +0100

    Indentation...

--- main.go
+++ main.go
@@ -74,7 +74,7 @@ func main() {
 		}),
 	)
 
-  // This needs to remain after log initialisation and before server initialisation.
+	// This needs to remain after log initialisation and before server initialisation.
 	ap := startAPM(ctx, cfgs)
 
 	healthResponder, err := startHealthResponder(ctx, probestatus, fatalErr)
